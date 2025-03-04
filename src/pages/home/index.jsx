953b0d9decefe8f3eb24366e4950dfb330fe0ea0
Victor-Watanabe <watavh@hotmail.com>
2025-03-04 12:51:56 -0300

    feat: User registration processing

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,47 +1,54 @@
-import './style.css'
-import Trash from "../../assets/trash.jpg"
+import { useRef, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';  
+import './style.css';
+import Trash from "../../assets/trash.jpg";
 
 function Home() {
+  const [users, setUsers] = useState([]);
 
-  const users = [
-    {
-      id: '1234',
-      name: 'Victor Watanabe',
-      age: '21 Anos',
-      email: 'Exemple@gmail.com'
-    },
-    {
-      id: '12345',
-      name: 'Lavínia Watanabe',
-      age: '19 Anos',
-      email: 'Exemple02@gmail.com'
-    }
-  ]
+  const inputName = useRef();
+  const inputAge = useRef();
+  const inputEmail = useRef();
+
+  function PressButton() {
+    const newUser = {
+      id: uuidv4(), 
+      name: inputName.current.value,
+      age: inputAge.current.value + " Anos",
+      email: inputEmail.current.value,
+    };
+
+    setUsers([...users, newUser]);
+
+    inputName.current.value = "";
+    inputAge.current.value = "";
+    inputEmail.current.value = "";
+  }
 
   return (
-    <div className='container'>
+    <div className="container">
       <form>
         <h1>Cadastro de usuários</h1>
-        <input placeholder='Nome' name='name' type='text' />
-        <input placeholder='Idade' name='age' type='number' />
-        <input placeholder='E-mail' name='email' type='email' />
-        <button type='button'>Cadastrar</button>
+        <input placeholder="Nome" name="name" type="text" ref={inputName} />
+        <input placeholder="Idade" name="age" type="number" ref={inputAge} />
+        <input placeholder="E-mail" name="email" type="email" ref={inputEmail} />
+        <button type="button" onClick={PressButton}>Cadastrar</button>
       </form>
 
       {users.map((user) => (
-        <div key={user.id} className='card'>
+        <div key={user.id} className="card">
           <div>
             <p>Nome: <span>{user.name}</span></p>
             <p>Idade: <span>{user.age}</span></p>
             <p>Email: <span>{user.email}</span></p>
           </div>
           <button>
-            <img src={Trash} />
+            <img src={Trash} alt="Excluir usuário" />
           </button>
         </div>
       ))}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
